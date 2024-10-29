db3aebf45b322a812179c0cffef61e171cf11797
João Conserva <boscoj@outlook.com>
2024-10-29 18:06:48 -0300

    chore: add route to create github data asset

---
+++ src/app/api/github/route.ts
@@ -0,0 +1,55 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { authApi } from '@/services/api/api';
+import { TypesAccessLevel } from '@/services/api/types';
+
+export async function POST(req: NextRequest) {
+  const { claim, did } = await req.json();
+  if (!claim || !did) {
+    return NextResponse.json('Claim or DID not found', { status: 400 });
+  }
+
+  const gatewayUserToken = process.env.GATEWAY_USER_TOKEN;
+  if (!gatewayUserToken) {
+    return NextResponse.json(
+      { error: 'Gateway user token not found' },
+      { status: 500 }
+    );
+  }
+
+  const githubDataModelId = process.env.GITHUB_DATA_MODEL_ID;
+  if (!githubDataModelId) {
+    return NextResponse.json(
+      { error: 'Github data model id not found' },
+      { status: 500 }
+    );
+  }
+
+  const body = {
+    claim: claim as any,
+    acl: [
+      {
+        address: did,
+        roles: [
+          TypesAccessLevel.RoleView,
+          TypesAccessLevel.RoleUpdate,
+          TypesAccessLevel.RoleDelete,
+          TypesAccessLevel.RoleShare,
+        ],
+      },
+    ],
+    name: 'GitHub Data',
+    data_model_id: parseInt(githubDataModelId),
+    tags: ['github', 'user'],
+  };
+
+  const { data, error } = await authApi(gatewayUserToken).POST('/data-assets', {
+    body: body as any,
+  });
+
+  if (error) {
+    return NextResponse.json(error, { status: 500 });
+  }
+
+  return NextResponse.json(data);
+}
