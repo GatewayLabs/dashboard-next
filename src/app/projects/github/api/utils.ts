2b3852a04ae5ef13d1ae44c735244361cbf15ab8
João Conserva <boscoj@outlook.com>
2024-10-31 19:32:50 -0300

    refactor: improve error handling and logging in getGatewayUserToken function

--- src/app/projects/github/api/utils.ts
+++ src/app/projects/github/api/utils.ts
@@ -2,42 +2,40 @@ let gatewayUserToken: string | null = null;
 
 export async function getGatewayUserToken(): Promise<string | null> {
   if (gatewayUserToken) {
-    if (!gatewayUserToken) {
-      throw new Error('Gateway user token is null');
-    }
     return gatewayUserToken;
   }
 
-  const url = process.env.NEXTAUTH_URL;
-  if (!url) {
-    console.error('NEXTAUTH_URL is not defined');
+  const baseURL = process.env.NEXTAUTH_URL;
+  if (!baseURL) {
+    console.error(
+      'Error: NEXTAUTH_URL is not defined in the environment variables'
+    );
     throw new Error('NEXTAUTH_URL is not defined');
   }
 
-  if (typeof window === 'undefined') {
-    try {
-      const gatewayLogin = await fetch(
-        `${url}/projects/github/api/login-gateway`
+  const fullURL = `${baseURL}/projects/github/api/login-gateway`;
+
+  try {
+    const gatewayLogin = await fetch(fullURL);
+    if (!gatewayLogin.ok) {
+      console.error(
+        `Error fetching login-gateway: Status ${gatewayLogin.status} - ${gatewayLogin.statusText}`
       );
-      if (!gatewayLogin.ok) {
-        console.error(`Error fetching login-gateway: ${gatewayLogin.status}`);
-        throw new Error(
-          `Failed to fetch login-gateway: ${gatewayLogin.statusText}`
-        );
-      }
-
-      const data = await gatewayLogin.json();
-      if (!data || !data.token) {
-        console.error('Gateway user token not found');
-        throw new Error('Gateway user token not found');
-      }
-
-      gatewayUserToken = data.token;
-    } catch (error) {
-      console.error('Error fetching gateway user token:', error);
-      throw new Error('Failed to obtain gateway user token');
+      throw new Error(
+        `Failed to fetch login-gateway: ${gatewayLogin.statusText}`
+      );
     }
-  }
 
-  return gatewayUserToken;
+    const data = await gatewayLogin.json();
+    if (!data || !data.token) {
+      console.error('Error: Gateway user token not found in response');
+      throw new Error('Gateway user token not found');
+    }
+
+    gatewayUserToken = data.token;
+    return gatewayUserToken;
+  } catch (error) {
+    console.error('Error fetching gateway user token:', error);
+    throw new Error('Failed to obtain gateway user token');
+  }
 }
