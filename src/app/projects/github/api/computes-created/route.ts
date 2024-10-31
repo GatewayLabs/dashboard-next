8c23a5bbc8d2e0d766bd2b2ef6eb87bcb2e4c0dd
Lucas (Kbooz) <lucas1inacio7souza@gmail.com>
2024-10-31 15:54:11 -0300

    chore: rollback

--- src/app/projects/github/api/computes-created/route.ts
+++ src/app/projects/github/api/computes-created/route.ts
@@ -2,9 +2,7 @@ import { NextResponse } from 'next/server';
 
 import { authApi } from '@/services/api/api';
 
-const api = authApi(process.env.GATEWAY_USER_TOKEN!, {
-  fetch: (request) => fetch({ ...request, cache: 'no-store' }),
-});
+const api = authApi(process.env.GATEWAY_USER_TOKEN!);
 
 export const GET = async () => {
   const { data, error } = await api.GET('/compute-requests/me');
