c985dce055bb494418a0d2916eed9fa1a9b5db07
João Conserva <boscoj@outlook.com>
2024-10-31 19:45:43 -0300

    refactor: remove hardcoded token return when skipping gateway token fetch during build

--- src/app/projects/github/api/utils.ts
+++ src/app/projects/github/api/utils.ts
@@ -5,11 +5,6 @@ export async function getGatewayUserToken(): Promise<string | null> {
     return gatewayUserToken;
   }
 
-  if (process.env.SKIP_AUTH_FETCH_DURING_BUILD === 'true') {
-    console.warn('Skipping gateway token fetch during build');
-    return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkaWQiOiJkaWQ6Z2F0ZXdheWlkOmdhdGV3YXk6OGQ1OTQ1YWQ4MjY4YzlhZjI2ZWMwNTkwYjMyNDM2OTQ1ZDBkNWY2NzQyOGQzMzNiNWYwMDJhMDNkZjA5NTVkOCIsImV4cCI6MTczMDY2NjQxMSwid2FsbGV0X2FkZHJlc3MiOiIweDlkRUM3ODcwY0JBNzVhQkM1NjI3YmY3MTM0NWFlZjMwOTVENGFjMmMifQ.gQcA-X1Mk3nED38B4Ts0rPy1vqJbm9PRLMdLpngQF1c';
-  }
-
   const baseURL = process.env.NEXTAUTH_URL;
   if (!baseURL) {
     console.error(
