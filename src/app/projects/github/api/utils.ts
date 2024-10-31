8c64e64458bccea26d15b72159874a3898b9abe9
João Conserva <boscoj@outlook.com>
2024-10-31 19:20:39 -0300

    refactor: enhance getGatewayUserToken function with improved error handling and null checks

--- src/app/projects/github/api/utils.ts
+++ src/app/projects/github/api/utils.ts
@@ -1,34 +1,43 @@
 let gatewayUserToken: string | null = null;
 
-export async function getGatewayUserToken(): Promise<string> {
+export async function getGatewayUserToken(): Promise<string | null> {
   if (gatewayUserToken) {
+    if (!gatewayUserToken) {
+      throw new Error('Gateway user token is null');
+    }
     return gatewayUserToken;
   }
 
   const url = process.env.NEXTAUTH_URL;
   if (!url) {
-    throw new Error('NEXTAUTH_URL is not defined in the environment variables');
+    console.error('NEXTAUTH_URL is not defined');
+    throw new Error('NEXTAUTH_URL is not defined');
   }
 
-  try {
-    const gatewayLogin = await fetch(
-      `${url}/projects/github/api/login-gateway`
-    );
-    if (!gatewayLogin.ok) {
-      throw new Error(
-        `Failed to fetch login-gateway: ${gatewayLogin.statusText}`
+  if (typeof window === 'undefined') {
+    try {
+      const gatewayLogin = await fetch(
+        `${url}/projects/github/api/login-gateway`
       );
-    }
+      if (!gatewayLogin.ok) {
+        console.error(`Error fetching login-gateway: ${gatewayLogin.status}`);
+        throw new Error(
+          `Failed to fetch login-gateway: ${gatewayLogin.statusText}`
+        );
+      }
 
-    const data = (await gatewayLogin.json()) as { token: string };
-    if (!data || !data.token) {
-      throw new Error('Gateway user token not found');
-    }
+      const data = await gatewayLogin.json();
+      if (!data || !data.token) {
+        console.error('Gateway user token not found');
+        throw new Error('Gateway user token not found');
+      }
 
-    gatewayUserToken = data.token;
-    return gatewayUserToken;
-  } catch (error) {
-    console.error('Error fetching gateway user token:', error);
-    throw new Error('Failed to obtain gateway user token');
+      gatewayUserToken = data.token;
+    } catch (error) {
+      console.error('Error fetching gateway user token:', error);
+      throw new Error('Failed to obtain gateway user token');
+    }
   }
+
+  return gatewayUserToken;
 }
